951a51138ea0bba783c60f45d0f1ee15cd51704d
Pritam-Khergade <pritamkherg8@gmail.com>
2022-08-12 11:40:08 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,88 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.25.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-2"
+  profile = "terraform-pritam"
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "this" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  key_name        = "terraform-key-aws"
+  vpc_security_group_ids = [aws_security_group.this.id]
+  user_data       = <<EOF
+#!/bin/bash
+sudo apt-get update -y 
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install openjdk-11-jre -y 
+sudo apt install default-jdk -y 
+sudo apt-get update -y 
+sudo apt-get install jenkins -y 
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+
+ EOF
+
+  tags = {
+    Name = "HelloWorld"
+  }
+
+}
+
+resource "aws_security_group" "this" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
